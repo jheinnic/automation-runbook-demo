bfeb38f4b18ec7ef0f35ed9080580aac899c7882
John Heinnickel <john.heinnickel@thermofisher.com>
2023-09-03 22:26:25 +0000

    IAM import

--- lib/stacks/CdkPipelineStack.ts
+++ lib/stacks/CdkPipelineStack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as iam from 'aws-cdk-lib/aws-iam'
 import * as pipelines from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { AutomationRunbookDemoStage } from '../AutomationRunbookDemoStage'
