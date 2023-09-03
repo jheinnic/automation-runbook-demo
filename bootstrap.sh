817cb2dc3ac780ea39600e30483d522288eb41ca
John Heinnickel <john.heinnickel@thermofisher.com>
2023-09-03 20:55:54 +0000

    Updating for @aws-cdk/core:newStyleStackSynthesis=true

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 
 export AWS_PROFILE=irtemp
 export AWS_REGION=us-east-1
-CDK_NEW_BOOTSTRAP=1 npx cdk bootstrap --profile irtemp -i --tags costcenter=10051227 --tags division=csd --tags environment=nonprod --tags application=datalake --tags app_role=demoSsm --qualifier demo-ssm --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess --toolkit-stack-name csd-demo-ssm --trust 811617080253 --trust-for-lookup 811617080253 --trust 284611682665 --trust-for-lookup 284611682665 --context tryctxt=tryvalue --context deployment=demo-ssm -v aws://284611682665/us-east-1 2>&1 | tee bootlog_2665.log
+CDK_NEW_BOOTSTRAP=1 npx cdk bootstrap --profile irtemp -i --tags costcenter=10051227 --tags division=csd --tags environment=nonprod --tags application=IonReporter --tags app_role=demoSsm --qualifier demo-ssm --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess --toolkit-stack-name csd-demo-ssm --trust 284611682665 --trust-for-lookup 284611682665 --context tryctxt=tryvalue --context deployment=demo-ssm -v aws://284611682665/us-east-1 2>&1 | tee bootlog2_2665.log
 
 export AWS_PROFILE=dltemp
 export AWS_PROFILE=default
 export AWS_REGION=us-east-1
-# CDK_NEW_BOOTSTRAP=1 npx cdk bootstrap --profile default -i --tags costcenter=10051227 --tags division=csd --tags environment=nonprod --tags application=datalake --tags app_role=demoSsm --qualifier demo-ssm --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess --toolkit-stack-name csd-demo-ssm --trust 284611682665 --trust-for-lookup 284611682665 --context tryctxt=tryvalue --context deployment=demo-ssm -v aws://811617080253/us-east-1 2>&1 | tee bootlog_0253.log
+CDK_NEW_BOOTSTRAP=1 npx cdk bootstrap --profile default -i --tags costcenter=10051227 --tags division=csd --tags environment=nonprod --tags application=IonReporter --tags app_role=demoSsm --qualifier demo-ssm --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess --toolkit-stack-name csd-demo-ssm --trust 284611682665 --trust-for-lookup 284611682665 --context tryctxt=tryvalue --context deployment=demo-ssm -v aws://811617080253/us-east-1 2>&1 | tee bootlog2_0253.log
