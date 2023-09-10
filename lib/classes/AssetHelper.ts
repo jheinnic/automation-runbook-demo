1e0343cd453b3cded37d94e0bcc27f654145314b
John Heinnickel <john.heinnickel@thermofisher.com>
2023-09-10 10:24:47 +0000

    Fix level of nesting for assets to account for having moved AssetHelper one package level deeper.

--- lib/classes/AssetHelper.ts
+++ lib/classes/AssetHelper.ts
@@ -1,16 +1,19 @@
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Code } from 'aws-cdk-lib/aws-lambda';
 import { Asset } from 'aws-cdk-lib/aws-s3-assets';
 import { Construct } from 'constructs';
-import * as path from 'path';
+import { join } from 'path';
+
+
+const ASSETS_ROOT = join(__dirname, '..', '..', 'assets')
 
 export class AssetHelper {
     static getLambdaScript(relativePath: string) {
-        return lambda.Code.fromAsset(path.join(__dirname, '..', 'assets', relativePath))
+        return Code.fromAsset(join(ASSETS_ROOT, relativePath))
     }
 
     static bindYamlFile(scope: Construct, id: string, relativePath: string): Asset {
         return new Asset(scope, id, {
-            path: path.join(__dirname, '..', 'assets', 'relativePath'),
+            path: join(ASSETS_ROOT, relativePath),
             deployTime: false
         });
     }
