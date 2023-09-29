64007e99a8ff192843a36bd1448e38a7eaa540ab
John Heinnickel <john.heinnickel@thermofisher.com>
2023-09-29 23:22:25 +0000

    FIx tag Aspect use

--- bin/create-pipeline.ts
+++ bin/create-pipeline.ts
@@ -1,11 +1,11 @@
-import * as cdk from 'aws-cdk-lib'
-import * as ec2 from 'aws-cdk-lib/aws-ec2'
-import * as iam from 'aws-cdk-lib/aws-iam'
+import { App, Environment } from 'aws-cdk-lib'
+import { Role } from 'aws-cdk-lib/aws-iam'
+
 import { CdkPipelineStack } from '../lib/stacks'
 
-const ciEnv: cdk.Environment = { account: '284611682665', region: 'us-east-1' }
-const env1: cdk.Environment = { account: '284611682665', region: 'us-east-1' }
-const env2: cdk.Environment = { account: '811617080253', region: 'us-east-1' }
+const ciEnv: Environment = { account: '284611682665', region: 'us-east-1' }
+const env1: Environment = { account: '284611682665', region: 'us-east-1' }
+const env2: Environment = { account: '811617080253', region: 'us-east-1' }
 
 /*
 const stackSynthesizer = new cdk.DefaultStackSynthesizer({
@@ -14,15 +14,13 @@ const stackSynthesizer = new cdk.DefaultStackSynthesizer({
 });
 */
 
-const app = new cdk.App();
-
-// iam.Role.customizeRoles(app, { preventSynthesis: false });
+const app = new App();
+Role.customizeRoles(app, { preventSynthesis: false });
 
 const pipeline = new CdkPipelineStack(app, 'bPipeline', {
     ciEnv: ciEnv, devEnv: env1, prodEnv: env2,
     // synthesizer: stackSynthesizer
 });
 
-
-
-app.synth();
+pipeline.synth()
+app.synth()
--- lib/stacks/CdkPipelineStack.ts
+++ lib/stacks/CdkPipelineStack.ts
@@ -1,93 +1,91 @@
-import * as cdk from 'aws-cdk-lib'
-import * as ec2 from 'aws-cdk-lib/aws-ec2'
-import * as iam from 'aws-cdk-lib/aws-iam'
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Environment, SecretValue, Stack, Tags } from 'aws-cdk-lib'
+import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+
 import { AutomationRunbookDemoStage } from '../AutomationRunbookDemoStage'
 
-export class CdkPipelineStack extends cdk.Stack {
-    // private codeSource: CodePipelineSource
-    // private pipeline: CodePipeline
+export class CdkPipelineStack extends Stack {
+    private codeSource: CodePipelineSource
+    private pipeline: CodePipeline
 
-    constructor(scope: Construct, id: string, props: CdkPipelineStackProps ) {
+    constructor(scope: Construct, id: string, props: CdkPipelineStackProps) {
         // provide your CI/CD account info here
-        super(scope, id, { env: props.ciEnv });
+        super(scope, id, { env: props.ciEnv })
 
         // const synthesizer: cdk.IStackSynthesizer = props.synthesizer;
 
-	const codeSource = CodePipelineSource.gitHub('john-heinnickel/automation-runbook-demo', 'main', {
-	authentication: cdk.SecretValue.secretsManager('csd/devops/ir_stack_pipeline')
-        });
-//            actionName: 'LoadSource',
-//            connectionArn: 'arn:aws:codestar-connections:us-east-1:284611682665:connection/5695ad23-6e33-4634-a035-cbd18b0d8499',
-//            codeBuildCloneOutput: false,
-//            triggerOnPush: true
-//        });
+        this.codeSource = CodePipelineSource.gitHub('john-heinnickel/automation-runbook-demo', 'main', {
+            authentication: SecretValue.secretsManager('csd/devops/ir_stack_pipeline')
+        })
 
-        const pipeline = new CodePipeline(this, 'aPipeline', {
+        this.pipeline = new CodePipeline(this, 'aPipeline', {
             // we need to activate this for cross account deployments
-            // pipelineName: 'Pipeline',
             crossAccountKeys: true,
             synth: new ShellStep('Synth', {
-                // configure source repo here
                 input: codeSource,
-                // configure build steps here
                 commands: ['npm ci', 'npm run build', 'npx cdk synth'],
             })
-	    /*
-            codeBuildDefaults: {
-                rolePolicy: [iam.PolicyStatement.fromJson({
-                    "Condition": {
-                        "ForAnyValue:StringEquals": {
-                            "iam:ResourceTag/aws-cdk:bootstrap-role": [
-                                "image-publishing",
-                                "file-publishing",
-                                "deploy"
-                            ]
-                        }
-                    },
-                    "Action": "sts:AssumeRole",
-                    "Resource": "arn:*:iam::811617080253:role/*",
-                    "Effect": "Allow"
-                })]
-            }
-	    */
-        });
+            /*
+                codeBuildDefaults: {
+                    rolePolicy: [iam.PolicyStatement.fromJson({
+                        "Condition": {
+                            "ForAnyValue:StringEquals": {
+                                "iam:ResourceTag/aws-cdk:bootstrap-role": [
+                                    "image-publishing",
+                                    "file-publishing",
+                                    "deploy"
+                                ]
+                            }
+                        },
+                        "Action": "sts:AssumeRole",
+                        "Resource": "arn:*:iam::811617080253:role/*",
+                        "Effect": "Allow"
+                    })]
+                }
+            */
+        })
     
         /*const stackSynthesizer = new cdk.DefaultStackSynthesizer({
             qualifier: 'demo-ssm',
             bootstrapStackVersionSsmParameter: '/cdk-bootstrap/demo-ssm/version'
         });*/
-        const wl1 = pipeline.addStage(
-            new AutomationRunbookDemoStage(this, 'DevWorkload', {
-                env: props.devEnv // , synthesizer: stackSynthesizer
-            }));
-        const wl2 = pipeline.addStage(
-            new AutomationRunbookDemoStage(this, 'ProdWorkload', {
-                env: props.prodEnv // , synthesizer: stackSynthesizer
-            }));
+        const wl1 = new AutomationRunbookDemoStage(this, 'DevWorkload', {
+            env: props.devEnv // , synthesizer: stackSynthesizer
+        })
+        const wl2 = new AutomationRunbookDemoStage(this, 'ProdWorkload', {
+            env: props.prodEnv // , synthesizer: stackSynthesizer
+        })
 
-        cdk.Tags.of(wl1).add('Environment', 'non-prod')
-        cdk.Tags.of(wl1).add('deployment', 'wl1')
+        Tags.of(wl1).add('Environment', 'nonprod')
+        Tags.of(wl1).add('deployment', 'wl1')
 
-        cdk.Tags.of(wl2).add('Environment', 'prod')
-        cdk.Tags.of(wl2).add('deployment', 'wl2')
+        Tags.of(wl2).add('Environment', 'prod')
+        Tags.of(wl2).add('deployment', 'wl2')
         
         // const imdsv2Aspect = new ec2.InstanceRequireImdsv2Aspect();
         // cdk.Aspects.of(wl1).add(imdsv2Aspect)
         // cdk.Aspects.of(wl2).add(imdsv2Aspect)
         [wl1, wl2].foreach(wl => {
-            cdk.Tags.of(wl).add('costcenter', '10051227')
-            cdk.Tags.of(wl).add('group', 'GSG')
-            cdk.Tags.of(wl).add('group_beneficiary', 'GSG')
-            cdk.Tags.of(wl).add('division', 'CSD')
-        });
+            Tags.of(wl).add('costcenter', '10051227')
+            Tags.of(wl).add('group', 'GSG')
+            Tags.of(wl).add('group_beneficiary', 'GSG')
+            Tags.of(wl).add('division', 'CSD')
+        })
+
+        this.pipeline.addStage(wl1)
+        this.pipeline.addStage(wl2, {
+            pre: new ManualApprovalStep('AcceptIt', { comment: 'Do you accept this?' })
+        })
+    }
+
+    public synth(): void {
+        this.pipeline.buildPipeline();
     }
 }
 
 export interface CdkPipelineStackProps {
-    ciEnv: cdk.Environment,
-    devEnv: cdk.Environment,
-    prodEnv: cdk.Environment
+    ciEnv: Environment,
+    devEnv: Environment,
+    prodEnv: Environment
     // synthesizer: cdk.IStackSynthesizer
 }
